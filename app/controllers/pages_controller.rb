4461c1ef170aeefa2d99c8acd99bb3dc6dc100fd
Yizhu ZONG <agnes.lain@gmail.com>
2020-08-10 12:32:19 +0200

    done

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,16 @@
+class PagesController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @question = params[:question]
+    @answer = nil
+    if @question.end_with?("?")
+      @answer = "Silly question, get dressed and go to work!"
+    elsif @question.eql?("I am going to work right now!")
+      @answer = "Great!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
